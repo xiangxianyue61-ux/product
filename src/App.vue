b3b87f7e9963439cb7659cf3d9b842c955b02358
岳相贤 <16432130+yue-xiangxian@user.noreply.gitee.com>
2026-01-22 16:34:27 +0800

    test: husky

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 console.log('test'); // 若配置了 'no-console': 'warn'，会出现警告
 const unusedVar = 'test'; // 若配置了 '@typescript-eslint/no-unused-vars': 'warn'，会出现警告
+const test: any = 1;
 </script>
 
 <template></template>
